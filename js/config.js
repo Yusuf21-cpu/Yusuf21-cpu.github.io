107a868784c77fc81978ec25aab92aefa4418a78
yusuf591-cpu <59925798+yusuf591-cpu@users.noreply.github.com>
2020-12-23 21:28:24 +0500

    Fix6

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ particlesJS(
   {
     "particles": {
       "number": {
-        "value": 150,
+        "value": 100,
         "density": {
           "enable": true,
           "value_area": 800
@@ -109,7 +109,7 @@ particlesJS(
         }
       }
     },
-    "retina_detect": true
+    "retina_detect": false
   }
 
 )
