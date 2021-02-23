2b1d068f15e23090cfb06944c5c1207b08c144f2
yusuf <yusufsaydullayev@gmail.com>
2021-02-23 21:42:07 +0500

    tiltjs

--- js/script.js
+++ js/script.js
@@ -3,5 +3,19 @@ $(document).ready(function () {
     new WOW().init();
 });
 
+VanillaTilt.init(document.querySelectorAll(".about-card"), {
+    max: 25,
+    speed: 500,
+    glare:true,
+    "max-glare":1
+});
+VanillaTilt.init(document.querySelectorAll(".flipInX"), {
+    max: 25,
+    speed: 500,
+    glare:true,
+    "max-glare":1
+});
+
+
 
 
